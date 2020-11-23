cc9a6708125fcc17614e8fc06d976340a9c86534
Gebhardt Benjamin <Benjamin.Gebhardt@elektrobit.com>
2020-11-23 15:19:01 +0100

    Setup Premake: - Add premake to the project - Generate the project via premake

---
+++ premake5.lua
@@ -0,0 +1,105 @@
+workspace "GraphicsEngine"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "GraphicsEngine"
+	location "GraphicsEngine"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs 
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"GE_PLATFORM_WINDOWS",
+			"GE_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "GE_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "GE_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "GE_Dist"
+		optimize "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"GraphicsEngine/vendor/spdlog/include",
+		"GraphicsEngine/src"
+	}
+
+	links
+	{
+		"GraphicsEngine"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"GE_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "GE_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "GE_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "GE_Dist"
+		optimize "On"
