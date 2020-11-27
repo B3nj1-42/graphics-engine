b0ffdbe5d11ff46aaca1a84e8f04ba5fb188eb79
Gebhardt Benjamin <Benjamin.Gebhardt@elektrobit.com>
2020-11-27 14:30:56 +0100

    Add Input polling: - Add a new input interface to poll the input events (Key- and MouseEvents - Implement the interface for the windows window

--- GraphicsEngine/src/Platform/Windows/WindowsWindow.h
+++ GraphicsEngine/src/Platform/Windows/WindowsWindow.h
@@ -22,6 +22,8 @@ namespace GraphicsEngine {
 		void SetVSync(bool enabled) override;
 		bool IsVSync() const override;
 
+		inline virtual void* GetNativeWindow() const { return m_Window; }
+
 	private:
 		virtual void Init(const WindowProperties& properties);
 		virtual void Shutdown();
